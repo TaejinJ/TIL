f088caae9be6e8009d1b4202dc0eaa6a80ed943f
taejin jo <xowls296@gmail.com>
2022-11-07 21:57:49 +0900

    중복된 배열인덱스 제거

---
+++ codewars/Unique_In_Order.js
@@ -0,0 +1,29 @@
+/**Implement the function unique_in_order which takes as argument a sequence and returns a list of items without any elements with the same value next to each other and preserving the original order of elements.
+
+For example:
+
+uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
+uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
+uniqueInOrder([1,2,2,3,3])       == [1,2,3]
+*/
+
+/**내풀이 */
+function uniqueInOrder(it) {
+  var result = [];
+  var last;
+
+  for (var i = 0; i < it.length; i++) {
+    if (it[i] !== last) {
+      result.push((last = it[i]));
+    }
+  }
+
+  return result;
+}
+/**Best Practice */
+// var uniqueInOrder = function (iter) {
+//   return [...iter].filter((a, i) => a !== iter[i - 1]);
+// };
+console.log(uniqueInOrder("AAAABBBCCDAABBB"));
+console.log(uniqueInOrder("ABBCcAD"));
+console.log(uniqueInOrder([1, 2, 2, 3, 3]));
