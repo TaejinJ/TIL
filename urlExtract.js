00c28278043e0b0686fe4d1d488e40c3fcfddca5
TaeJin Jo <90677238+TaejinJ@users.noreply.github.com>
2022-11-22 11:13:51 +0900

    Update urlExtract.js

--- urlExtract.js
+++ urlExtract.js
@@ -1,7 +1,15 @@
+//**정규식 사용 */
 function domainName(url){
     return url.match(/(?:http(?:s)?:\/\/)?(?:w{3}\.)?([^\.]+)/i)[1];
   }
+/**문자열 잘라서 추출  2번출력시 원하는 도메인네임을 추출 할 수 없음.*/
+//   function domainName(url){
+//    url = url.replace("http://","").replace("https//","").replace("www.","");
+//    return url.split(".")[0];
+//   }
 
+//1
  console.log( domainName("http://github.com/carbonfive/raygun"));
+ //2
  console.log( domainName("http://mynamewww.com"));
  
