bd25f54050aa5dedd7ec181c53933ab82adbc34e
TaeJin Jo <90677238+TaejinJ@users.noreply.github.com>
2022-11-23 23:48:00 +0900

    Update TwoSum.js

--- codewars/TwoSum.js
+++ codewars/TwoSum.js
@@ -15,4 +15,15 @@ function twoSum(numbers, target) {
 
 }
 
+
+/**map을 사용 한 코드  */
+// function twoSum(numbers, target) {
+//     let seen = new Map();
+//     for (let i = 0; i < numbers.length; i++) {
+//         let x = numbers[i], y = target - x;
+//         if (seen.has(y))
+//             return [seen.get(y), i];
+//         seen.set(x, i);
+//     }
+// }
 twoSum([1, 2, 3, 4, 5], 3); // [0,1]
