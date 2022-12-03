c6889871090ab5a3144c814e361974c5daf5752b
taejin jo <xowls296@gmail.com>
2022-12-03 21:55:28 +0900

    Rot13

---
+++ codewars/Rot13.js
@@ -0,0 +1,6 @@
+function rot13(str) {
+    let input = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    let output = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+    return str.replace(/[a-z]/gi, (check) => { return output[input.indexOf(check)] });
+}
+console.log(rot13("hello my name is taejin!!!!"));
