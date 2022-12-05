7c70c739948d80fca91992c10a37b934a59bd2b8
taejin jo <xowls296@gmail.com>
2022-12-06 00:16:25 +0900

    HashTag

--- codewars/HashTag.js
+++ codewars/HashTag.js
@@ -1,10 +1,11 @@
 function generateHashtag(str) {
-    if (str === "" || str.length > 141) {
-        return false;
-    }
-    let change = str.replace(/(\s*)/g, "");
-    return "#" + change;
+    let res = '#' + str.split(' ')
+        .map(word => word.charAt(0)
+            .toUpperCase() + word.slice(1))
+        .join('');
+    return res = res.length > 140 || res.trim() == '' || res == '#' ? false : res;
 }
+
 console.log(generateHashtag("h          ello"));
 console.log(generateHashtag("h          e        ll           o"));
 console.log(generateHashtag(" Hello there thanks for trying my Kata"));
