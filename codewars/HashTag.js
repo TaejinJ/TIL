f9a207bbeecf5087e60ad4b1f03ded63c76b1de4
taejin jo <xowls296@gmail.com>
2022-12-06 00:01:40 +0900

    fix

--- codewars/HashTag.js
+++ codewars/HashTag.js
@@ -1,10 +1,13 @@
 function generateHashtag(str) {
-    if (str === "") {
+    if (str === "" || str.length > 141) {
         return false;
     }
     let change = str.replace(/(\s*)/g, "");
-    return "#"+change;
+    return "#" + change;
 }
 console.log(generateHashtag("h          ello"));
 console.log(generateHashtag("h          e        ll           o"));
 console.log(generateHashtag(" Hello there thanks for trying my Kata"));
+console.log(generateHashtag(" Hello there thanks for trying my Katasddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd"));
+
+
