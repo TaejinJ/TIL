defdef6a150a4d76fa3443e23adac3416af7b828
taejin jo <xowls296@gmail.com>
2022-12-06 00:36:38 +0900

    again

--- codewars/HashTag.js
+++ codewars/HashTag.js
@@ -3,12 +3,24 @@ function generateHashtag(str) {
         .map(word => word.charAt(0)
             .toUpperCase() + word.slice(1))
         .join('');
+
     return res = res.length > 140 || res.trim() == '' || res == '#' ? false : res;
 }
 
+
+// 다시짜봄 
+function generateHashtag(str) {
+    const output = str.trim().length > 0 &&
+        '#' + str.split(' ').map(word => word && (word[0].toUpperCase() + word.slice(1))).join('');
+
+    return output.length < 141 && output;
+}
+
+
+
+
+
+
 console.log(generateHashtag("h          ello"));
-console.log(generateHashtag("h          e        ll           o"));
-console.log(generateHashtag(" Hello there thanks for trying my Kata"));
-console.log(generateHashtag(" Hello there thanks for trying my Katasddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd"));
 
 
