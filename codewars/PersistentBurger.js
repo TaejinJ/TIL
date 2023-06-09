6905131460aeef90fa5d0849a7df93ad8eaa8912
taejin jo <xowls296@gmail.com>
2023-06-09 17:51:20 +0900

    persistentBurger

---
+++ codewars/PersistentBurger.js
@@ -0,0 +1,18 @@
+function persistence(num) {
+    // 횟수 count
+    let count = 0;
+
+    // 10이상일때만 돌게 while문 작성
+    while (num >= 10) {
+
+        // num을 배열로 만들어서 ex) (["1","3","4"]) 이런식 reduce로 다 곱해주고 count++,  그걸 num에 다시 반환 예를들어 9가 되면 num은 10미만이기에 
+        // 탈출하고 count를 리턴
+        num = String(num)
+            .split("")
+            .reduce((a, b) => a * b);
+
+        count++;
+    }
+
+    return count;
+}
