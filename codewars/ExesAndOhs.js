5b1667414aa987c20f282d93f5369908d0b476c3
taejin jo <xowls296@gmail.com>
2023-06-27 23:20:24 +0900

    다른방법추가

--- codewars/ExesAndOhs.js
+++ codewars/ExesAndOhs.js
@@ -1,21 +1,35 @@
-function XO(str) {
-    // 대문자로 변환
-    str = str.toUpperCase();
+// function XO(str) {
+//     // 대문자로 변환
+//     str = str.toUpperCase();
   
-    // 'o'와 'x'의 개수 세기
-    let countO = 0;
-    let countX = 0;
-    for (let i = 0; i < str.length; i++) {
-      if (str[i] === 'O') {
-        countO++;
-      } else if (str[i] === 'X') {
-        countX++;
-      }
+//     // 'o'와 'x'의 개수 세기
+//     let countO = 0;
+//     let countX = 0;
+//     for (let i = 0; i < str.length; i++) {
+//       if (str[i] === 'O') {
+//         countO++;
+//       } else if (str[i] === 'X') {
+//         countX++;
+//       }
+//     }
+  
+//     // 개수 비교
+//     return countO === countX;
+//   }
+  
+//   console.log(XO("ooxx")); // true
+//   console.log(XO("ozzo")); // false
+
+  function XO(str) {
+      let sum = 0;
+      for(var i=0; i<str.length; i++){
+        //1. x가 있을때 1더하기 
+      if(str[i].toLowerCase() === 'x') sum--;
+      //2. o가 있을때 -1하기
+      // ex) "xo" 0++ --; (0+1-1 = 0)
+      if(str[i].toLowerCase() === 'o') sum++;
     }
-  
-    // 개수 비교
-    return countO === countX;
+    return sum == 0;
   }
-  
   console.log(XO("ooxx")); // true
-  console.log(XO("ozzo")); // true
+  console.log(XO("zzooxxx")); // false
