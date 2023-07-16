e69a625ca64aa09785cadf4db62e4c91854ba6b7
taejin jo <xowls296@gmail.com>
2023-07-16 21:36:11 +0900

    가장높은점수를가진단어 반환

--- codewars/HightestScoringWord.js
+++ codewars/HightestScoringWord.js
@@ -10,3 +10,21 @@
 
 // All letters will be lowercase and all inputs will be valid.
 
+function high(x) {
+    // 입력 문자열을 분할하여 배열을 생성.
+    let wordList = x.split(' ');
+    
+    // 각 단어의 점수를 계산하여 배열로 저장.
+    let scores = wordList.map(word => {
+      
+      let score = [...word].reduce((sum, char) => sum + char.charCodeAt(0) - 96, 0);
+      return score;
+    });
+  
+    // 가장 높은 점수를 가진 단어의 인덱스를 찾는다..
+    let highestIndex = scores.indexOf(Math.max(...scores));
+  
+    // 가장 높은 점수를 가진 단어를 반환.
+    return wordList[highestIndex];
+  }
+  
